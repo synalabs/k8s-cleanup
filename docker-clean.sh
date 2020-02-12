d44f3c245456924c15aff7fe3b9c411c07b666c1
Benjamin PREVOST <bprevost@synalabs.com>
2020-02-12 18:18:22 +0100

    Change docker cleanup

--- docker-clean.sh
+++ docker-clean.sh
@@ -2,11 +2,13 @@
 
 while true; do
   # Remove exited containers
-  docker ps -a -q -f status=exited    | xargs --no-run-if-empty docker rm -v
+  # docker ps -a -q -f status=exited    | xargs --no-run-if-empty docker rm -v
   # Remove dangling images
-  docker images -f "dangling=true" -q | xargs --no-run-if-empty docker rmi
+  # docker images -f "dangling=true" -q | xargs --no-run-if-empty docker rmi
   # Remove dangling volumes
-  docker volume ls -qf dangling=true  | xargs --no-run-if-empty docker volume rm
+  # docker volume ls -qf dangling=true  | xargs --no-run-if-empty docker volume rm
+  # Prune all image
+  docker image prune -a -f 
 
   # DOCKER_CLEAN_INTERVAL defaults to 30min
   sleep $DOCKER_CLEAN_INTERVAL
